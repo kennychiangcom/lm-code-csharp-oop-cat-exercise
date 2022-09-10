83d3311f6c9dc66edbc052f376e1b93238b394f0
kennychiangcom <kenny@chiang.com>
2022-09-10 14:27:46 +0100

    First succesfully tested build

--- OopCatExercise/Cat.cs
+++ OopCatExercise/Cat.cs
@@ -3,6 +3,38 @@ namespace OopCatExercise
 {
 	public abstract class Cat : ICat
 	{
-
-	}
+    }
+    public class DomesticCat : Cat
+    {
+        public bool IsAsleep = false;
+        public string Setting = "domestic";
+        public int AverageHeight = 23;
+        public void GoToSleep()
+        {
+            IsAsleep = true;
+        }
+        public void WakeUp()
+        {
+            IsAsleep = false;
+        }
+        public string Eat()
+        {
+            return "Purrrrrrr";
+        }
+    }
+    public class LionCat : Cat
+    {
+        public int AverageHeight = 1100;
+        public string Eat()
+        {
+            return "Roar!!!!";
+        }
+    }
+    public class CheetahCat : Cat
+    {
+        public string Eat()
+        {
+            return "Zzzzzzz";
+        }
+    }
 }
