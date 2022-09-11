7969234d06f4a5fe361846b0602b44d329d16649
kennychiangcom <kenny@chiang.com>
2022-09-11 17:44:11 +0100

    Implemented interface and abstraction class, and used abstraction, encapsulation and inheritance

--- OopCatExercise/Cat.cs
+++ OopCatExercise/Cat.cs
@@ -3,36 +3,50 @@ namespace OopCatExercise
 {
 	public abstract class Cat : ICat
 	{
-    }
-    public class DomesticCat : Cat
-    {
-        public bool IsAsleep = false;
+        private bool AsleepStatus;
+
         public string Setting = "domestic";
         public int AverageHeight = 23;
+
+        public abstract string Eat();
+
+        public bool IsAsleep
+        {
+            get { return AsleepStatus; }
+            set { AsleepStatus = value; }
+        }
+
         public void GoToSleep()
         {
             IsAsleep = true;
         }
+
         public void WakeUp()
         {
             IsAsleep = false;
         }
-        public string Eat()
+    }
+    public class DomesticCat : Cat
+    {
+        public override string Eat()
         {
             return "Purrrrrrr";
+            //return "It will do I suppose";
         }
     }
     public class LionCat : Cat
     {
-        public int AverageHeight = 1100;
-        public string Eat()
+        public new int AverageHeight = 1100;
+        public new string Setting = "wild";
+        public override string Eat()
         {
             return "Roar!!!!";
         }
     }
     public class CheetahCat : Cat
     {
-        public string Eat()
+        public new string Setting = "wild";
+        public override string Eat()
         {
             return "Zzzzzzz";
         }
--- OopCatExercise/ICat.cs
+++ OopCatExercise/ICat.cs
@@ -3,5 +3,8 @@ namespace OopCatExercise
 {
 	public interface ICat
 	{
-	}
+        public string Eat();
+        void GoToSleep();
+        void WakeUp();
+    }
 }
